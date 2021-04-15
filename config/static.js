9ddad6f3f77a6cbd24b8479d87f52f491251a22c
Miko <jherjati@gmail.com>
2021-04-15 10:59:24 +0700

    refactor fastify-static config

---
+++ config/static.js
@@ -0,0 +1,16 @@
+module.exports = {
+  public: {
+    root: path.join(__dirname, "public"),
+    prefix: "/",
+  },
+  assets: {
+    root: path.join(__dirname, "public/assets"),
+    prefix: "/assets",
+    decorateReply: false,
+  },
+  forms: {
+    root: path.join(__dirname, "public/forms"),
+    prefix: "/forms",
+    decorateReply: false,
+  },
+};
