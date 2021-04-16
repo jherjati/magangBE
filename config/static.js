b2b52323e7908b8f30424a4060d71cd8122e4b63
Miko <jherjati@gmail.com>
2021-04-16 16:42:47 +0700

    erase unnecessary register

--- config/static.js
+++ config/static.js
@@ -5,14 +5,4 @@ module.exports = {
     root: path.join(__dirname, "../public"),
     prefix: "/",
   },
-  publicAssets: {
-    root: path.join(__dirname, "../public/assets"),
-    prefix: "/assets",
-    decorateReply: false,
-  },
-  publicForms: {
-    root: path.join(__dirname, "../public/forms"),
-    prefix: "/forms",
-    decorateReply: false,
-  },
 };
--- index.js
+++ index.js
@@ -3,14 +3,6 @@ const fastify = require("fastify")({ logger: true });
 
 // Register core plugins
 fastify.register(require("fastify-static"), require("./config/static").public);
-fastify.register(
-  require("fastify-static"),
-  require("./config/static").publicAssets
-);
-fastify.register(
-  require("fastify-static"),
-  require("./config/static").publicForms
-);
 
 // Register custom routes (route included)
 fastify.register(require("./routes/static"));
