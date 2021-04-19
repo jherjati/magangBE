8911864e4126526a189cdb24e5723f28c596bc71
Miko <jherjati@gmail.com>
2021-04-19 17:09:10 +0700

    add ejs

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ const fastify = require("fastify")({ logger: true });
 
 // Register core plugins
 fastify.register(require("fastify-static"), require("./config/static").public);
+fastify.register(require("point-of-view"), {
+  engine: {
+    ejs: require("ejs"),
+  },
+});
 
 // Register custom routes (route included)
 fastify.register(require("./routes/static"));
+fastify.register(require("./routes/ssr"));
 
 // Run the server!
 const start = async () => {
