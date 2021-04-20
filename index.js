58d79006732c302b329558ffcd90593df11060b6
Miko <jherjati@gmail.com>
2021-04-20 10:31:25 +0700

    local data in json and env vars

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // Require the framework and instantiate it
 const fastify = require("fastify")({ logger: true });
 
+if (process.env.NODE_ENV !== "production")
+  require("dotenv").config(require("./config/env").options.dotenv);
+
 // Register core plugins
+fastify.register(require("fastify-env"), require("./config/env").options);
 fastify.register(require("fastify-static"), require("./config/static").public);
 fastify.register(require("point-of-view"), {
   engine: {
@@ -16,7 +20,7 @@ fastify.register(require("./routes/ssr"));
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen(process.env.PORT || 5000, "0.0.0.0");
+    await fastify.listen(process.env.PORT, "0.0.0.0");
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
